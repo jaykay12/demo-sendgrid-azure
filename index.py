87bd14972710f9df84802286dba23761657cb87a
jaykay12 <jalazkumar1208@gmail.com>
2018-10-11 00:49:35 +0530

    Finished Demo App

--- index.py
+++ index.py
@@ -1,17 +1,20 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, render_template
 import sendgrid
 import os
 from sendgrid.helpers.mail import *
 
 app = Flask(__name__) 
   
-@app.route('/success') 
-def success(self): 
-   return 'Mail Sent!'
+@app.route('/success/<code>') 
+def success(code): 
+   return 'Mail Sent!: %s' % code
+@app.route('/failure/<code>')
+def failure(code):
+	return 'Problem Sending Mail!: %s' % code
 
-@app.route('/failure')
-def failure(self):
-	return 'Problem Sending Mail!' 
+@app.route('/')
+def loadForm():
+	return render_template('index.html')
   
 @app.route('/send',methods = ['POST', 'GET']) 
 def sendMail():
@@ -33,10 +36,10 @@ def sendMail():
 	print(response.body)
 	print(response.headers)
 
-	if response.status_code == 200:
-		return redirect(url_for('success'))
+	if response.status_code == 202:
+		return redirect(url_for('success', code = str(response.status_code)))
 	else:
-		return redirect(url_for('failure'))
+		return redirect(url_for('failure',code = str(response.status_code)))
 
        
   
